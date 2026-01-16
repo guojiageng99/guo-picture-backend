0f351ec6cf38db0d375ee954b6edd13d9c40c3d4
gjg <guojiageng1024@163.com>
2026-01-16 16:21:43 +0800

    用户传图

--- src/main/java/com/guo/guopicturebackend/service/PictureService.java
+++ src/main/java/com/guo/guopicturebackend/service/PictureService.java
@@ -1,11 +1,18 @@
 package com.guo.guopicturebackend.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.guo.guopicturebackend.model.dto.picture.PictureQueryRequest;
+import com.guo.guopicturebackend.model.dto.picture.PictureReviewRequest;
+import com.guo.guopicturebackend.model.dto.picture.PictureUploadByBatchRequest;
 import com.guo.guopicturebackend.model.dto.picture.PictureUploadRequest;
 import com.guo.guopicturebackend.model.entity.Picture;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.guo.guopicturebackend.model.entity.User;
 import com.guo.guopicturebackend.model.vo.PictureVO;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
 * @author 44884
@@ -17,15 +24,48 @@ public interface PictureService extends IService<Picture> {
     /**
      * 上传图片
      *
-     * @param multipartFile
+     * @param inputSource
      * @param pictureUploadRequest
      * @param loginUser
      * @return
      */
-    PictureVO uploadPicture(MultipartFile multipartFile,
+    PictureVO uploadPicture(Object inputSource,
                             PictureUploadRequest pictureUploadRequest,
                             User loginUser);
 
+
+
+    public QueryWrapper<Picture> getQueryWrapper(PictureQueryRequest pictureQueryRequest);
+
+    public PictureVO getPictureVO(Picture picture, HttpServletRequest request);
+
+    public Page<PictureVO> getPictureVOPage(Page<Picture> picturePage, HttpServletRequest request);
+
+    public void validPicture(Picture picture);
+
+    /**
+     * 图片审核
+     *
+     * @param pictureReviewRequest
+     * @param loginUser
+     */
+    void doPictureReview(PictureReviewRequest pictureReviewRequest, User loginUser);
+
+
+    public void fillReviewParams(Picture picture, User loginUser);
+
+
+    /**
+     * 批量抓取和创建图片
+     *
+     * @param pictureUploadByBatchRequest
+     * @param loginUser
+     * @return 成功创建的图片数
+     */
+    Integer uploadPictureByBatch(
+            PictureUploadByBatchRequest pictureUploadByBatchRequest,
+            User loginUser
+    );
 
 
 }
--- src/main/java/com/guo/guopicturebackend/service/UserService.java
+++ src/main/java/com/guo/guopicturebackend/service/UserService.java
@@ -71,4 +71,13 @@ public interface UserService extends IService<User> {
 
     public QueryWrapper<User> getQueryWrapper(UserQueryRequest userQueryRequest);
 
+    /**
+     * 是否为管理员
+     *
+     * @param user
+     * @return
+     */
+    boolean isAdmin(User user);
+
+
 }
