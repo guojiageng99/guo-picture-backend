954a42a14973a6edc80f68b57c3c3268852dd7d9
gjg <guojiageng1024@163.com>
2026-03-13 10:46:40 +0800

    database configuration update

--- src/main/java/com/guo/guopicturebackend/api/aliyun/AliYunAiApi.java
+++ src/main/java/com/guo/guopicturebackend/api/aliyun/AliYunAiApi.java
@@ -11,8 +11,8 @@ import com.guo.guopicturebackend.api.aliyun.model.CreateOutPaintingTaskResponse;
 import com.guo.guopicturebackend.api.aliyun.model.GetOutPaintingTaskResponse;
 import com.guo.guopicturebackend.exception.BusinessException;
 import com.guo.guopicturebackend.exception.ErrorCode;
-import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Slf4j
